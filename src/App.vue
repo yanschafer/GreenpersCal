5f97d8a64547d8f9b7f9650a9bb2512e13cf84eb
yanschafer1488 <yan@psyreply.com>
2023-02-14 22:59:25 +0300

    bytton added

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,22 @@
   <img class="logo" src="./assets/logo.png">
   <CalendarComponent>
   </CalendarComponent>
-  <div class="row">
+  <div class="row legend-row">
     <div class="col legend">
-      <div class="container legend-day"><i class="fa-solid fa-sun icon-day"></i></div>
+      <button class="container legend-day"><i class="fa-solid fa-sun icon-day"></i></button>
       <h6 class="legend-heading">День</h6>
     </div>
     <div class="col legend">
-      <div class="container legend-night"><i class="fa-solid fa-moon icon-night"></i></div>
+      <button class="container legend-night"><i class="fa-solid fa-moon icon-night"></i></button>
       <h6 class="legend-heading">Ночь</h6>
     </div>
+
   </div>
+  <div class="row button-save-row">
+  <div class="col button-save-col">
+    <button type="button" class="btn btn-primary">Сохранить</button>
+  </div>
+</div>
 </template>
 
 <script>
@@ -27,6 +33,11 @@ export default {
 </script>
 
 <style>
+.legend-row {
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+}
 @import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap');
 #app {
   font-family: 'Open Sans', sans-serif;
@@ -35,6 +46,36 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.btn {
+  width: 90vw;
+  background-color: #96b963;
+  border: 1px solid #f1ffdb;
+}
+.btn:hover {
+  width: 90vw;
+  background-color: #7e9857!important;
+  border: 1px solid #8aa85a;
+}
+.btn:focus {
+  width: 90vw;
+  background-color: #7e9857!important;
+  border: 1px solid #8aa85a;
+}
+.btn:active {
+  width: 90vw;
+  background-color: #7e9857;
+  border: 1px solid #8aa85a;
+}
+.button-save-col {
+  display: flex;
+  justify-content: center;
+}
+.button-save-row {
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
 }
 .legend {
   display: flex;
@@ -48,6 +89,7 @@ export default {
 .legend-day {
   background: #619fa8;
   color: #619fa8;
+  border: none;
   padding: 0;
   height: 2rem;
   width: 2rem;
@@ -70,12 +112,25 @@ export default {
   color: #619fa8;
   transition: all 1s ease
 }
+.legend-day:focus-within {
+  background: transparent;
+  border: 1px solid #619fa8;
+  color: #619fa8;
+  transition: all 1s ease
+}
+.legend-day:active {
+  background: transparent;
+  border: 1px solid #619fa8;
+  color: #619fa8;
+  transition: all 1s ease
+}
 
 .legend-night {
   background: #283b42;
   color: #283b42;
   padding: 0;
   height: 2rem;
+  border: none;
   width: 2rem;
   margin-left: 0;
   margin-right: 1rem;
@@ -91,6 +146,18 @@ export default {
   transition: all 1s ease;
 }
 .legend-night:focus {
+  background: transparent;
+  border: 1px solid #283b42;
+  color: #283b42;
+  transition: all 1s ease
+}
+.legend-night:focus-within {
+  background: transparent;
+  border: 1px solid #283b42;
+  color: #283b42;
+  transition: all 1s ease
+}
+.legend-night:active {
   background: transparent;
   border: 1px solid #283b42;
   color: #283b42;
